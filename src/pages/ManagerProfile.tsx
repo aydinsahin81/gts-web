30a4d9334cd9bead97a76fd1d150e25f44b0ae55
aydinsahin81 <aydin@mt-teknoloji.com>
2025-04-05 16:16:49 +0300

    manager profil düzeltme

--- src/pages/ManagerProfile.tsx
+++ src/pages/ManagerProfile.tsx
@@ -4,7 +4,14 @@ import { Box } from '@mui/material';
 
 const ManagerProfilePage: React.FC = () => {
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ 
+      display: 'flex', 
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: '100%',
+      height: '100vh'
+    }}>
       <ManagerProfile />
     </Box>
   );
